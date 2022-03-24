9209dd068a1a5639764845f6d790b97a74609096
CHSFN <christoferferreira28@gmail.com>
2022-03-24 11:06:08 -0300

    v2.01

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function criarParagrafo() {
+function numeros1a10() {
 
     let para = document.createElement('p');
-    para.textContent = "Parabens vc ganhou um ford K";
+    para.textContent = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10";
     document.body.appendChild(para);
     
 }
+function texto() {
 
-const botoes = document.querySelectorAll('button');
+    let url = location.href
+    location.href = "vendas.html"
 
-for(var i = 0; i < botoes.length; i++){
-    botoes[i].addEventListener('click', criarParagrafo)
 }
+
+function redirecionar() {
+    window.location.href = "https://www.devmedia.com.br/jenkins-e-sonar-desenvolvimento-agil-em-java-revista-devcast-magazine-2/23642#:~:text=Ambas%20s%C3%A3o%20aplica%C3%A7%C3%B5es%20Open%20Source,Sonar%20um%20dashboard%20de%20qualidade."
+}
+
+
